727fe22ff43ee0f59e1e6e698c67744d77e1ac06
Subhadip <mnhacker2001@gmail.com>
2023-08-09 06:19:05 +0530

    fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Footer from "./Footer";
 import dynamic from "next/dynamic";
-const NavBar = dynamic(() => import("./NavBar"), { ssr: false });
+const NavBar = dynamic(() => import("./NavBar.jsx"), { ssr: false });
 
 export default function Layout({ children }) {
   return (
