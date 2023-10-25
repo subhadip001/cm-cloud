a20d0a0912cad2ccce1f92a9afb26070066f6f9f
Subhadip <mnhacker2001@gmail.com>
2023-10-25 09:34:03 +0530

    creds to false

--- utils/axiosGoogle.js
+++ utils/axiosGoogle.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const axiosGoogleClient = axios.create({
-  baseURL: "https://api.cyphermanager.com", // Replace with your server's URL
-  withCredentials: true, // Include credentials (cookies) in the requests
+  baseURL: "https://api.cyphermanager.com", 
+  withCredentials: false, 
 });
 
 export default axiosGoogleClient;
